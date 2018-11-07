deb17cb6ace512b10282637ec152f78c65ac23ca
Momoko Ishimura <applemelody03@gmail.com>
2018-11-07 22:09:01 +0900

    first commit .

---
+++ Spring Batch/src/test/java/jp/co/test/ExampleItemReaderTests.java
@@ -0,0 +1,20 @@
+package jp.co.test; 
+
+import jp.co.test.ExampleItemReader;
+
+import junit.framework.TestCase;
+
+public class ExampleItemReaderTests extends TestCase {
+
+	private ExampleItemReader reader = new ExampleItemReader();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", reader.read());
+	}
+
+	public void testReadTwice() throws Exception {
+		reader.read();
+		assertEquals(null, reader.read());
+	}
+
+}
---
+++ Spring Batch/src/test/java/jp/co/test/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package jp.co.test;
+
+import jp.co.test.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
